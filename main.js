84fb9c34bc0a58f5fb29460168ed016a3f2178ef
RestonLimousine <49453345+RestonLimousine@users.noreply.github.com>
2019-04-09 14:39:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-samsaraAPI = {};
+var accessToken;
 
-samsaraAPI.sendReq = function (uri, cb, params) {
+var sendReq = function (uri, cb, params) {
   var req = new XMLHttpRequest();
   req.addEventListener("load", function () {
     var rsp = this.responseText;
     cb(rsp);
   });
-  uri = "https://api.samsara.com/v1" + uri + "?access_token=" + samsaraAPI.accessToken;
+  uri = "https://api.samsara.com/v1" + uri + "?access_token=" + accessToken;
   uri = uri + (params ? "&" + params.map(function (x) { return x.join("="); }).join("&") : "");
   req.open("GET", uri);
   req.send();
 };
 
-samsaraAPI.out = [];
+var out = [];
 
-samsaraAPI.getDrivers = function () {
-  samsaraAPI.sendReq("/fleet/drivers", function (x) {
+var getDrivers = function () {
+  sendReq("/fleet/drivers", function (x) {
     x = JSON.parse(x).drivers;
+    var t = (new Date).getTime();
     for (var i = 0; i < x.length; i++){
-      samsaraAPI.sendReq(
+      out[i] = x[i];
+      sendReq(
         "/fleet/hos_authentication_logs",
         function (y) {
-          samsaraAPI.out.push(y);
-          if (samsaraAPI.out.length === x.length) {
-            console.log(samsaraAPI.out.length);
+          out[i].authenticationLogs = JSON.parse(y).authenticationLogs;
+          if (out.length === x.length) {
+            console.log(out.length);
           }
         },
         [["driverId", x[i].id],
-         ["startMs", (new Date).getTime()-(24*60*60*1000)],
-         ["endMs", (new Date).getTime()]]
+         ["startMs", t-(24*60*60*1000)],
+         ["endMs", t]]
       );
     }
   });
 }
+
+
