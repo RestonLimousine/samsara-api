ee0bef58e9dea7b138140da6055aa148f380acb3
RestonLimousine <49453345+RestonLimousine@users.noreply.github.com>
2019-04-09 14:07:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-function sendReq (at, uri, cb, params) {
+samsaraAPI = {};
+
+samsaraAPI.sendReq = function (uri, cb, params) {
   var req = new XMLHttpRequest();
   req.addEventListener("load", function () {
     var rsp = this.responseText;
     cb(rsp);
   });
-  uri = "https://api.samsara.com/v1" + uri + "?access_token=" + at;
+  uri = "https://api.samsara.com/v1" + uri + "?access_token=" + samsaraAPI.accessToken;
   uri = uri + (params ? "&" + params.map(function (x) { return x.join("="); }).join("&") : "");
   req.open("GET", uri);
   req.send();
 };
 
-var out = [];
+samsaraAPI.out = [];
 
-function getDrivers (at) {
-  sendReq(at, "/fleet/drivers", function (x) {
+samsaraAPI.getDrivers = function () {
+  sendReq("/fleet/drivers", function (x) {
     x = JSON.parse(x).drivers;
     for (var i = 0; i < x.length; i++){
-      sendReq(at,
+      sendReq(
         "/fleet/hos_authentication_logs",
         function (y) {
           out.push(y);
