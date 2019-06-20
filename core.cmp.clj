59ca99d10e1c5cf9a2c4b2493335ae292adf717c
RestonLimousine <49453345+RestonLimousine@users.noreply.github.com>
2019-06-20 12:21:49 -0400

    Update core.cmp.clj

--- core.cmp.clj
+++ core.cmp.clj
@@ -2,3 +2,7 @@
  :require [[element.core :as el :src "https://raw.githubusercontent.com/fofx-software/element/master/core.cmp.clj"]]}
 
 (el/set-props el/body {:inner-html "Hello World!"})
+
+(.appendChild el/head
+  (el/create-element
+    ["script" {:src "https://unpkg.com/xlsx@0.14.2/dist/xlsx.full.min.js"}]))
