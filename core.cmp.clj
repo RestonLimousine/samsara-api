edb3f18866e43a03185370f915bd000b418ca04c
RestonLimousine <49453345+RestonLimousine@users.noreply.github.com>
2019-06-20 12:14:35 -0400

    Update core.cmp.clj

--- core.cmp.clj
+++ core.cmp.clj
@@ -1,4 +1,4 @@
 {:ns samsara-api.core
- :require [[element.core :loc "https://raw.githubusercontent.com/fofx-software/element/master/core.cmp.clj"]]}
-(pr "done")
-(element.core/set-props element.core/body {:inner-html "Hello World!"})
+ :require [[element.core :as el :src "https://raw.githubusercontent.com/fofx-software/element/master/core.cmp.clj"]]}
+
+(el/set-props el/body {:inner-html "Hello World!"})
