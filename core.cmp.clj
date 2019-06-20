0fc2f198278ad1cb93d360b5b48645f35f680e76
RestonLimousine <49453345+RestonLimousine@users.noreply.github.com>
2019-06-20 13:20:32 -0400

    Update core.cmp.clj

--- core.cmp.clj
+++ core.cmp.clj
@@ -1,7 +1,7 @@
 {:ns samsara-api.core
  :require [[element.core :as el :src "https://raw.githubusercontent.com/fofx-software/element/master/core.cmp.clj"]]}
 
-(el/set-props el/body {:inner-html "Hello World!"})
+(el/set-props el/body {"innerHTML" "Hello World!"})
 
 (.appendChild el/head
   (el/create-element
